5083398d59d7eea66c9a337f253da2c30b97379c
Jean Gabriel <jeangabriel180@hotmail.com>
2024-07-06 16:32:36 -0300

    first commit

---
+++ src/main/java/com/alura/scpegasus/ScpegasusApplication.java
@@ -0,0 +1,13 @@
+package com.alura.scpegasus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpegasusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScpegasusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/scpegasus/ScpegasusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.scpegasus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpegasusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
